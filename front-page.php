7af104e0368154dcf528c2ba997aa9bb17a114d9
Lisa <lisaqdebona@gmail.com>
2020-04-01 01:01:04 -0600

    added theme files

--- front-page.php
+++ front-page.php
@@ -1,8 +1,7 @@
 <?php get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		asdfasfsaf
-		
+
 		<?php while ( have_posts() ) : the_post();
 
 			get_template_part( 'template-parts/content', 'page' );
