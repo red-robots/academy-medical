997df36e9d430eb24f152197b56831dca6609e6c
Lisa <lisaqdebona@gmail.com>
2020-04-08 01:16:16 -0600

    Styling Solutions page

--- page.php
+++ page.php
@@ -12,18 +12,92 @@
  * @package bellaworks
  */
 
-get_header(); ?>
+get_header(); 
+$header_image = get_field("header_image"); 
+$has_header_image = ($header_image) ? 'has-header-image':'no-header-image';
+?>
 
-<div id="primary" class="content-area default cf">
-	<main id="main" class="site-main wrapper" role="main">
+<div id="primary" class="content-area default cf <?php echo $has_header_image ?>">
+	<main id="main" class="site-main cf" role="main">
 
-		<?php
-		while ( have_posts() ) : the_post();
+		<?php while ( have_posts() ) : the_post(); ?>
+			
+			<section class="maintext cf">
+				<div class="midwrap">
+					<?php the_content(); ?>
+				</div>
+				
+			</section>
 
-			get_template_part( 'parts/content', 'page' );
+			<?php  
+			$sections = get_field("twoColContent");
+			if($sections) { ?>
+			<section class="twocol-content cf">
+				<?php $i=1; foreach ($sections as $s) { 
+				$title = $s['col1'];
+				$text = $s['col2'];
+				$bluebox = $s['bluebox'];
+				$class = ($title && $text) ? 'twocol':'full';
+				if($i==1) {
+					$class .= ' first';
+				}
+				?>
+				<div class="contentrow <?php echo $class ?>">
+					<div class="wrapper">
+						<div class="flexwrap cf">
+							<?php if ($title) { ?>
+							<div class="col title">
+								<h2 class="hd"><?php echo $title ?></h2>
+							</div>	
+							<?php } ?>
+							<?php if ($text) { ?>
+							<div class="col text">
+								<?php echo $text ?>
+							</div>	
+							<?php } ?>
+						</div>
+					</div>
 
-		endwhile; // End of the loop.
-		?>
+					<?php if ($bluebox) { ?>
+					<div class="bluebox-area cf">
+						<div class="wrapper">
+							<div class="bluebox">
+								<?php echo $bluebox ?>
+							</div>
+						</div>
+					</div>	
+					<?php } ?>
+				</div>	
+				<?php $i++; } ?>
+			</section>
+			<?php } ?>
+
+			<?php 
+				$bottomtext = get_field("bottom_text"); 
+				$bottomImg = get_field("bottom_image");
+				$text1 =  (isset($bottomtext['text1']) && $bottomtext['text1']) ? $bottomtext['text1'] : '';
+				$text2 =  (isset($bottomtext['text2']) && $bottomtext['text2']) ? $bottomtext['text2'] : '';
+				$bottomStyle = ($bottomImg) ? ' style="background-image:url('.$bottomImg['url'].')"':'';
+			?>
+
+			<?php if ($bottomtext) { ?>
+			<section class="bottom-section cf"<?php echo $bottomStyle ?>>
+				<div class="wrapper">
+					<div class="textcol">
+						<?php if ($text1) { ?>
+						<h3 class="txt1"><?php echo $text1 ?></h3>	
+						<?php } ?>
+						<?php if ($text2) { ?>
+						<p class="txt2"><?php echo $text2 ?></p>	
+						<?php } ?>
+					</div>
+				</div>
+			</section>
+			<?php } ?>
+
+			
+
+		<?php endwhile; ?>
 
 	</main><!-- #main -->
 </div><!-- #primary -->
--- parts/banner.php
+++ parts/banner.php
@@ -2,38 +2,58 @@
 $banner = get_field("banner");
 $taglines = get_field("taglines");
 $placeholder = THEMEURI . 'images/rectangle.png';
-if($banner) { ?>
-<div class="banner cf">
-	<div class="bimage" style="background-image:url('<?php echo $banner['url'] ?>')">
-		<img src="<?php echo $placeholder ?>" alt="" aria-hidden="true" class="placeholder" />
-		<?php 
-		$slidesId = (count($taglines)>1) ? 'slideshow':'static-banner';
-		if ($taglines) { ?>
-		<div class="bcaption">
-			<div id="<?php echo $slidesId ?>" class="slideTextContainer wrapper swiper-container">
-				<div class="swiper-wrapper">
-					<?php foreach ($taglines as $tag) { ?>
-						<div class="swiper-slide">
-							<img src="<?php echo $placeholder ?>" alt="" aria-hidden="true" class="placeholder" />
-				    		<div class="inner-text">
-				    			<div class="textwrap">
-						    		<?php if ($tag['large_text']) { ?>
-						    		<h2 class="largeTxt"><?php echo $tag['large_text'] ?></h2>	
-						    		<?php } ?>
-						    		<?php if ($tag['small_text']) { ?>
-						    		<div class="smallTxt"><?php echo $tag['small_text'] ?></div>	
-						    		<?php } ?>
+if( is_front_page() || is_home() ) {
+	if($banner) { ?>
+	<div class="banner cf">
+		<div class="bimage" style="background-image:url('<?php echo $banner['url'] ?>')">
+			<img src="<?php echo $placeholder ?>" alt="" aria-hidden="true" class="placeholder" />
+			<?php 
+			$slidesId = (count($taglines)>1) ? 'slideshow':'static-banner';
+			if ($taglines) { ?>
+			<div class="bcaption">
+				<div id="<?php echo $slidesId ?>" class="slideTextContainer wrapper swiper-container">
+					<div class="swiper-wrapper">
+						<?php foreach ($taglines as $tag) { ?>
+							<div class="swiper-slide">
+								<img src="<?php echo $placeholder ?>" alt="" aria-hidden="true" class="placeholder" />
+					    		<div class="inner-text">
+					    			<div class="textwrap">
+							    		<?php if ($tag['large_text']) { ?>
+							    		<h2 class="largeTxt"><?php echo $tag['large_text'] ?></h2>	
+							    		<?php } ?>
+							    		<?php if ($tag['small_text']) { ?>
+							    		<div class="smallTxt"><?php echo $tag['small_text'] ?></div>	
+							    		<?php } ?>
+						    		</div>
 					    		</div>
-				    		</div>
-				    	</div>
-					<?php } ?>
+					    	</div>
+						<?php } ?>
+					</div>
 				</div>
+			</div>	
+			<?php } ?>
+		</div>
+		<div class="scrolldown">
+			<div class="wrapper"><a href="#content" title="Scroll Down" id="scrolldown"><span class="arrow"></span></a></div>
+		</div>
+	</div>
+	<?php } ?>
+
+<?php } else { ?>
+
+	<?php 
+	$header_image = get_field("header_image"); 
+	$page_title = get_the_title();
+	$style = ($header_image) ? ' style="background-image:url('.$header_image['url'].')"':'';
+	?>
+
+	<div class="hero cf"<?php echo $style ?>>
+		<div class="inner">
+			<div class="wrapper">
+				<div class="titlediv"><h1 class="pagetitle"><span><?php echo $page_title ?></span></h1></div>
 			</div>
-		</div>	
-		<?php } ?>
-	</div>
-	<div class="scrolldown">
-		<div class="wrapper"><a href="#content" title="Scroll Down" id="scrolldown"><span class="arrow"></span></a></div>
-	</div>
-</div>
+		</div>
+	</div>	
+	
+
 <?php } ?>
